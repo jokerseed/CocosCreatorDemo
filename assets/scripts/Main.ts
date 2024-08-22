9c552846c17a921291012b08ce72d1182d781876
jokerseed <573202560@qq.com>
2024-08-22 18:32:05 +0800

    物理

--- assets/scripts/Main.ts
+++ assets/scripts/Main.ts
@@ -1,4 +1,4 @@
-import { _decorator, assetManager, Canvas, Component, director, Node, UITransform } from 'cc';
+import { _decorator, assetManager, Canvas, Component, director, EPhysics2DDrawFlags, Node, PhysicsSystem2D, UITransform } from 'cc';
 import { ConfigManager } from './core/ConfigManager';
 import { ResManager } from './core/ResManager';
 import { QuadTree } from './core/QuadTree';
@@ -11,6 +11,12 @@ export class Main extends Component {
     }
 
     protected start() {
+        PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.Aabb |
+            EPhysics2DDrawFlags.Pair |
+            EPhysics2DDrawFlags.CenterOfMass |
+            EPhysics2DDrawFlags.Joint |
+            EPhysics2DDrawFlags.Shape;
+
         ConfigManager.instance.init();
         ResManager.instance.init();
         QuadTree.instance.init(64, 2);
